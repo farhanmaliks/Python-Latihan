975993262447a3d84e9325c21b09ad2033672fbe
razak <razak@cs.ui.ac.id>
2024-03-07 13:03:48 +0700

    Soal FOr

---
+++ soal_for.py
@@ -0,0 +1,13 @@
+# input [1, 2, 3, 4, 5]
+# output [1, 4, 9, 16, 25]
+
+# Buatlah function untuk menerima input lalu mengeluarkan output dengan kuadratu
+def squares(int_list):
+    # Code dibawah sini
+    # --------------------------------
+
+
+    # --------------------------------
+
+list_number = [1, 2, 3, 4, 5]
+print(squares(list_number))
